2a0dbb7d0b66b98ccea515c30590a2d7d12f98e1
Nic Doelger <doelgern@gmail.com>
2025-05-19 13:41:24 -0400

    post w/ validation

--- resources/csv.py
+++ resources/csv.py
@@ -8,22 +8,21 @@ import json
 
 from schemas import CSVSchema, CSVUpdateSchema
 
-blp = Blueprint("CSVs", __name__, description="User Updated CSV")
+blp = Blueprint("CSVs", __name__, description="Client Updated CSV")
 
 
 @blp.route("/<string:user_id>")
-class CSVList(MethodView):
+class CSVList(MethodView): # GET LIST OF CLIENT CSVS
     def get(self, user_id):
         try:
-            # Setup s3 client
+            # SETUP S3 CLIENT
             boto3.setup_default_session(profile_name="default")
             s3 = boto3.client("s3")
 
             objects_array = []
 
-            for key in s3.list_objects(
-                Bucket="vistar-dc", Prefix=f"2025/01/client-uploads/{user_id}"
-            )["Contents"]:
+            # APPEND EACH CSV INTO OBJECTS_ARRAY AND RETURN TO CLIENT
+            for key in s3.list_objects(Bucket="vistar-dc", Prefix=f"2025/01/client-uploads/{user_id}")["Contents"]:
                 objects_array.append(key["Key"])
 
             return objects_array
@@ -33,55 +32,55 @@ class CSVList(MethodView):
 
 @blp.route("/")
 class CSV(MethodView):
-    def post(self):
+    @blp.arguments(CSVSchema)
+    def post(self, upload_data): # ADD CSV TO CLIENT BUCKET
         try:
-            upload_data = request.get_json()
-
             print(upload_data)
 
             json_to_upload = json.dumps(upload_data["json"])
 
-            # Setup s3 client
+            # SETUP S3 CLIENT
             boto3.setup_default_session(profile_name="default")
-
             s3 = boto3.client("s3")
+            
             s3.put_object(
                 Body=json_to_upload,
                 Bucket="vistar-dc",
-                Key=f"2025/01/client-uploads/{upload_data['userId']}/{upload_data['title']}.json",
+                Key=f"2025/01/client-uploads/{upload_data['user_id']}/{upload_data['title']}.json",
             )
 
             return upload_data
 
         except Exception as e:
             print(f"Error: {str(e)}")
-    def put(self):
+   
+    def put(self): # UPDATE TITLE/JSON OF EXISTING CSV
         try:
             upload_data = request.get_json()
 
             print(upload_data)
             json_to_upload = json.dumps(upload_data["newJson"])
 
-            # Setup s3 client
+            # SETUP S3 CLIENT
             boto3.setup_default_session(profile_name="default")
             s3 = boto3.client("s3")
 
             # Need access to
             # s3.copy_object(
             #     Bucket="vistar-dc",
-            #     CopySource=f"2025/01/client-uploads/{upload_data['userId']}/{upload_data['oldName']}.json",
-            #     Key=f"2025/01/client-uploads/{upload_data['userId']}/{upload_data['newName']}.json",
+            #     CopySource=f"2025/01/client-uploads/{upload_data['user_id']}/{upload_data['oldName']}.json",
+            #     Key=f"2025/01/client-uploads/{upload_data['user_id']}/{upload_data['newName']}.json",
             # )
 
             # s3.delete_object(
             #     Bucket="vistar-dc",
-            #     Key=f"2025/01/client-uploads/{upload_data['userId']}/{upload_data['oldName']}.json",
+            #     Key=f"2025/01/client-uploads/{upload_data['user_id']}/{upload_data['oldName']}.json",
             # )
 
             s3.put_object(
                 Body=json_to_upload,
                 Bucket="vistar-dc",
-                Key=f"2025/01/client-uploads/{upload_data['userId']}/{upload_data['title']}.json",
+                Key=f"2025/01/client-uploads/{upload_data['user_id']}/{upload_data['title']}.json",
             )
 
             return "success"
--- schemas.py
+++ schemas.py
@@ -3,10 +3,12 @@ from marshmallow import Schema, fields
 
 class CSVSchema(Schema):
     title = fields.Str(required=True)
-    id = fields.Str(required=True)
+    user_id = fields.Str(required=True)
     json = fields.Str(required=True)
 
 class CSVUpdateSchema(Schema):
-    title = fields.Str()
-    json = fields.Str()
+    oldTitle = fields.Str()
+    newTitle = fields.Str()
+    new_json = fields.Str()
+    user_id = fields.Str(required=True)
 
